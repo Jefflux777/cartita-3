2ae4f5e2856aa255f3762fdbed8c2f3cd9d42472
Jefflux777 <sicajau.jefferson.ipcs@gmail.com>
2025-02-01 23:40:49 -0600

    index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const title = document.querySelector('.title')
-const text = HOLA!DANNITA'.split('')
-const text = 'De parte mia yeff ¿QUIERES SER MI SAN VALENTIN? <3'.split('')
+const text = 'HOLA!DANNITA'.split('')
+const text = 'De parte mia yeff Â¿QUIERES SER MI SAN VALENTIN? <3'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
